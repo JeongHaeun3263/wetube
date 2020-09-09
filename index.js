d90dac64d5aab7967930cf98d63fcee35f6951cc
JeongHaeun3263 <jhe3263@gmail.com>
2020-09-09 19:04:57 -0400

    created server, install babel

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hi');
+import express from 'express';
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on:http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hi, from home');
+const handleProfile = (req, res) => res.send('You are on my profile');
+
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
