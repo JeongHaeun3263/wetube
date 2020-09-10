69cc949a01f526b7f3d78dfd07f0579cd9d125d0
JeongHaeun3263 <jhe3263@gmail.com>
2020-09-09 21:53:04 -0400

    added middleware

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
 const app = express();
 
 const PORT = 4000;
@@ -7,8 +12,17 @@ const handleListening = () =>
   console.log(`Listening on:http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send('Hi, from home');
+
 const handleProfile = (req, res) => res.send('You are on my profile');
 
+// Middleware
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Route
 app.get('/', handleHome);
 
 app.get('/profile', handleProfile);
