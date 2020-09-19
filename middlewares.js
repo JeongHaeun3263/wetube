8acebb1b47080e995810aab574230f43b9672b93
JeongHaeun3263 <jhe3263@gmail.com>
2020-09-19 11:09:54 -0400

    login, profile controllers

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,10 @@ import routes from './routes';
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'WeTube';
   res.locals.routes = routes;
+  // for the test
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1,
+  };
   next();
 };
