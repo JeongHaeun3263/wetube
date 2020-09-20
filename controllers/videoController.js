84b75f1d040d0dc89e050beb2e6d1936c522ffd1
JeongHaeun3263 <jhe3263@gmail.com>
2020-09-20 13:56:31 -0400

    handled uploading video/creating video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
 import routes from '../routes';
+import Video from '../models/Videos';
 
-export const home = (req, res) => {
-  res.render('home', { pageTitle: 'Home', videos });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render('home', { pageTitle: 'Home', videos });
+  } catch (error) {
+    console.log(error);
+    const videos = await Video.find({});
+    res.render('home', { pageTitle: 'Home', videos: [] });
+  }
 };
 
 export const search = (req, res) => {
@@ -18,12 +26,17 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render('upload', { pageTitle: 'Upload' });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description },
+    body: { title, description },
+    file: { path },
   } = req;
-  // To Do: Upload and Save Video
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
